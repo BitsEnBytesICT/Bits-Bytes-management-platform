2bacaf36038537a016485bbf748057b6b19f46b2
remon zander <remon.zander1@gmail.com>
2026-05-20 15:59:39 +0200

    some small fixes

--- frontend/src/components/layout/navbar/navbar.tsx
+++ frontend/src/components/layout/navbar/navbar.tsx
@@ -4,21 +4,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons/faCircleArrowRight';
 import { faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons/faCircleArrowLeft';
 
-// -left-[180px]
-
 export default function Navbar(){
     const [isOpen, setIsOpen] = useState(false);
-
 
     return (
         <>
         <div className={`left-slider flex flex-col absolute top-0 transition-all duration-400 ${isOpen ? "-left-[150px]" : "-left-[280px]"}`}>
-            <div className="self-end">
+            <div className="navbar-content self-end">
                 <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? <FontAwesomeIcon size="2x" icon={faCircleArrowLeft} /> : <FontAwesomeIcon size="2x" icon={faCircleArrowRight} />}</button>
             </div>
         </div>
         <div className={`right-slider flex flex-col-reverse absolute bottom-0 transition-all duration-400 ${isOpen ? "-right-[160px]" : "-right-[240px]"}`}>
-            <div>
+            <div className="navbar-content">
                 <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? <FontAwesomeIcon size="2x" icon={faCircleArrowLeft} /> : <FontAwesomeIcon size="2x" icon={faCircleArrowRight} />}</button>
             </div>
         </div>
