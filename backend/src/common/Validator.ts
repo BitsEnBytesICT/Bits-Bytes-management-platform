f9b11407e4970196c6212e37f2c774e44c6680a0
remon zander <remon.zander1@gmail.com>
2026-05-20 13:27:22 +0200

    added types and updates to validator

--- backend/src/common/Validator.ts
+++ backend/src/common/Validator.ts
@@ -14,12 +14,12 @@ export const Fun = <input, output>(actual: (_: input) => output): Fun<input, out
 
 export interface IError {
     date: Date,
-    errorMSG: string | any
+    errorMSG: Error
 };
 
 export type Result<a, b> = (
-    | { kind: "error", key: keyof a, errorMSG: IError }
-    | { kind: "success", key: keyof a, result?: b });
+    | { kind: "error", key: keyof a, value?: b, errorMSG: IError }
+    | { kind: "success", key: keyof a, value?: b });
 
 type AllKeysValidators<a> = { [K in keyof a]: [K, Fun<a[K], boolean>, string] }[keyof a];
 type HasAllKeys<a, T extends AllKeysValidators<a>[]> = [keyof a] extends [T[number][0]] ? T : never;
@@ -28,43 +28,52 @@ export const pipe = <a extends object, T extends AllKeysValidators<a>[]>(data: a
     (args as Array<[keyof a, Fun<any, boolean>, string]>).forEach((func) => {
         const [key, validator, errorMSG] = func;
         try {
-            if (validator(data[key])) results.push({ kind: "success", result: data[key], key });
-            else results.push({ kind: "error", key, errorMSG: { date: new Date(), errorMSG } });
-        } catch (error) {
-            results.push({ kind: "error", key, errorMSG: { date: new Date(), errorMSG: error } });
+            if (validator(data[key])) results.push({ kind: "success", value: data[key], key });
+            else results.push({ kind: "error", key, value: data[key], errorMSG: { date: new Date(), errorMSG: new Error(errorMSG) } });
+        } catch (error: any) {
+            results.push({ kind: "error", key, value: data[key], errorMSG: { date: new Date(), errorMSG: new Error(error) } });
         }
     });
     return results;
 };
 
+// export const validationFactory = <a>(func: (_: a) => boolean) => {
+//     return Fun<a, [a,boolean]>(func);
+// }
 
 
+// validationFactory((name: string) => name.length > 0)
 
 
 //usage:
-//interface User {
+
+// interface User {
 //    name: string;
 //    age?: number;
-//}
+// }
 
-//const validateName = Fun<string, boolean>(name => name.length > 0);
-//const validateAge = Fun<number | undefined, boolean>(age => age == undefined ? true : age >= 0);
+// const validateNameUndefined = Fun<string, [string, boolean]>(name => name === undefined ? [name, false] : [name, true]);
+// const validateNameNew = Fun<[string, boolean], boolean>(name => name[1] ? true : name[0].length > 0);
+// const validateName = Fun<string, boolean>(name => name.length > 0);
+// const validateAge = Fun<number | undefined, boolean>(age => age == undefined ? true : age >= 0);
 
-//const user: User = { name: "Alice", age: 30 };
-//const results = pipe(user, ["name", validateName, "Name is empty"], ["age", validateAge, "Age is negative"]);
+// const user: User = { name: "Alice", age: 30 };
+// const results = pipe(user, ["name", validateNameUndefined.then(validateNameNew), "Name is empty"], ["age", validateAge, "Age is negative"]);
 
-//const user2: User = { name: "Bob" };
-//const results2 = pipe(user2, ["name", validateName, "Name is empty"], ["age", validateAge, "Age is negative"]);
+// const user2: User = { name: "Bob" };
+// const results2 = pipe(user2, ["name", validateName, "Name is empty"], ["age", validateAge, "Age is negative"]);
 
-//const user3: User = { name: "", age: 20 };
-//const results3 = pipe(user3, ["name", validateName, "Name is empty"], ["age", validateAge, "Age is negative"]);
+// const user3: User = { name: "", age: 20 };
+// const results3 = pipe(user3, ["name", validateName, "Name is empty"], ["age", validateAge, "Age is negative"]);
 
-//const user4: User = { name: "Rob", age: -10 };
-//const results4 = pipe(user4, ["name", validateName, "Name is empty"], ["age", validateAge, "Age is negative"]);
+// const user4: User = { name: "Rob", age: -10 };
+// const results4 = pipe(user4, ["name", validateName, "Name is empty"], ["age", validateAge, "Age is negative"]);
 
-//console.log(results);
-//console.log(results2);
-//console.log(results3);
-//console.log(results4);
+// console.log(results);
+// console.log(results2);
+// console.log(results3);
+// console.log(results4);
 
-//if (results.find((r) => r.kind == "error")) console.log("error found!!!"); //check for any error
+// results.forEach((r) => {
+//     if (r.kind === "error") throw r.errorMSG.errorMSG;
+// })
