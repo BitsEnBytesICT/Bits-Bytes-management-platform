7937570aae86de1f7dcca2bcf1df6556c8ab0fe6
remon zander <remon.zander1@gmail.com>
2026-07-01 14:51:45 +0200

    small fix to migrations and update to deployment yaml

--- backend/migrations/001-create-initial-schema.ts
+++ backend/migrations/001-create-initial-schema.ts
@@ -73,7 +73,7 @@ const upStatements: string[] = [
     `
     CREATE TABLE IF NOT EXISTS Attendances (
         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        participantsID INT UNSIGNED NOT NULL,
+        participantID INT UNSIGNED NOT NULL,
         clockinDate DATETIME(3) NOT NULL,
         clockoutDate DATETIME(3) NULL,
         workDuration INT NULL,
@@ -86,7 +86,7 @@ const upStatements: string[] = [
         ),
 
         CONSTRAINT fk_attendances_participant
-            FOREIGN KEY (participantsID)
+            FOREIGN KEY (participantID)
             REFERENCES Participants(id)
     ) ENGINE=InnoDB
     DEFAULT CHARSET=utf8mb4
